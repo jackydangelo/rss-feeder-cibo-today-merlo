4b325a2d1605d704ed725b481abdd0fd61793eec
Giacomo D'angelo <118721242+jackydangelo@users.noreply.github.com>
2026-05-23 23:40:35 +0200

    Update FEED_URL to point to the correct RSS feed

--- generate_feed.py
+++ generate_feed.py
@@ -7,7 +7,7 @@ import json
 import os
 
 AUTHOR_URL = "https://www.cibotoday.it/author/profile/davide-merlo/49729159100110/"
-FEED_URL = "https://jackydangelo.github.io/rss-feeder-cibo-today-merlo/feed.xml"
+FEED_URL = "https://jackydangelo.github.io/rss-feeder-cibo-today-merlo/rss.xml"
 
 MAX_ARTICLES = 20
 base_date = datetime(2025, 1, 1, tzinfo=timezone.utc)
