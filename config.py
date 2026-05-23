169975fa83c7efda2ba1950397dd8d013bb0f433
Giacomo D'angelo <118721242+jackydangelo@users.noreply.github.com>
2026-05-24 00:27:07 +0200

    Add configuration variables for site and feed URLs

---
+++ config.py
@@ -0,0 +1,5 @@
+AUTHOR_URL = "https://www.cibotoday.it/author/profile/davide-merlo/49729159100110/"
+FEED_URL = "https://jackydangelo.github.io/rss-feeder-cibo-today-merlo/rss.xml"
+SITE_URL = "https://www.cibotoday.it"
+MAX_ARTICLES = 20
+base_date = datetime(2025, 1, 1, tzinfo=timezone.utc)
