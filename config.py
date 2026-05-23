9b534ec2b0e9a3708833572c17932b33b931b365
Giacomo D'angelo <118721242+jackydangelo@users.noreply.github.com>
2026-05-24 00:40:00 +0200

    Add RSS title and description to config

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ from datetime import datetime,timezone
 AUTHOR_URL = "https://www.cibotoday.it/author/profile/davide-merlo/49729159100110/"
 FEED_URL = "https://jackydangelo.github.io/rss-feeder-cibo-today-merlo/rss.xml"
 SITE_URL = "https://www.cibotoday.it"
+TITLE_RSS = "Davide Merlo - CiboToday"
+DESCRIPTION_RSS = "Feed RSS personalizzato"
 MAX_ARTICLES = 20
+TIMEOUT = 6000
 base_date = datetime(2025, 1, 1, tzinfo=timezone.utc)
